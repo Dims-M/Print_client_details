98ddccad2d3a5b2aa3f5c1c0faded42e9bcbfc39
Дмитрий <rabolan@mail.ru>
2019-07-17 18:06:58 +0300

    1235

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -79,15 +79,25 @@ namespace ConsoleApp
 
         public void connektDB()
         {
-        String dbFileName;
-        SQLiteConnection m_dbConn  = new SQLiteConnection(); ;
-        SQLiteCommand m_sqlCmd = m_sqlCmd = new SQLiteCommand(); 
-
-        string db = "mobiles.db";
+            const string databaseName = @"mobiles.db;
+            SQLiteConnection connection =
+            new SQLiteConnection(string.Format("Data Source={0};", databaseName));
+            connection.Open();
+            SQLiteCommand command = new SQLiteCommand("SELECT name FROM sqlite_master WHERE type='table' ORDER BY name;", connection);
+            SQLiteDataReader reader = command.ExecuteReader();
+            foreach (DbDataRecord record in reader)
+                Console.WriteLine("Таблица: " + record["name"]);
+            connection.Close();
+            Console.WriteLine("Готово");
+            Console.ReadKey(true);
+            string db = "mobiles.db";
 
         }
 
 
+        /// <summary>
+        /// Метод для считывания страниц таблиц гугла
+        /// </summary>
         public static void test2()
         {
             UserCredential credential;
